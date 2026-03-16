bf949a9eae1a726c25e499216ee63a120dd69f81
ObsidianHaki <loickgandonou03@icloud.com>
2026-03-16 11:15:27 +0100

    upload documents enabled

--- src/services/document_service.py
+++ src/services/document_service.py
@@ -1,11 +1,22 @@
 import hashlib
-from typing import Collection
+from typing import BinaryIO, Collection
+
+from fastapi import UploadFile
 from ..db.collection import get_or_create_collection
+from .file_parser import parse_pdf
 
 collection=get_or_create_collection()
 
 def store_documents (documents:list[str]):
     for document in documents:
+        collection.add(
+        ids=hashlib.sha256(document.encode()).hexdigest(),
+        documents= document
+    )
+
+def store_pdf(documents_binary:UploadFile):
+     documents=parse_pdf(documents_binary.file)
+     for document in documents:
         collection.add(
         ids=hashlib.sha256(document.encode()).hexdigest(),
         documents= document
--- src/services/file_parser.py
+++ src/services/file_parser.py
@@ -1,2 +1,9 @@
-# Extract text from uploaded files (PDF, DOCX, TXT)
-# TODO: implement file parsing logic
+from typing import BinaryIO
+
+from pypdf import PdfReader
+
+def parse_pdf(file_strean:BinaryIO) :
+    reader=PdfReader(file_strean)
+    return [page.extract_text() for page in reader.pages]
+
+
